ede13e7309e9d854f249359822882afa6b398da7
Ross Parsons <Parsons_ross@ne.bah.com>
2022-04-21 17:23:14 -0400

    added kafka dependency to project

---
+++ core/src/main/java/org/spyne/javapluginquickstart/core/ConsumeMessages.java
@@ -0,0 +1,61 @@
+package org.spyne.javapluginquickstart.core;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumeMessages {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumeMessages.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kakfa Consumer");
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "java-plugin-quickstart-application";
+        String topic = "java-plugin-quickstart";
+
+        // 1. Create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // ConsumerConfig.AUTO_OFFSET_RESET_CONFIG
+        // none - if no previous offsets are found then don't even start
+        // earliest - read from the very beginning of the topic
+        // latest - read only from the 'now' of the topic
+
+        // 2. Create Consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        // 3. Subscribe consumer to our topic(s)
+        consumer.subscribe(Collections.singletonList(topic));
+
+        // 4. Poll for new data
+        while (true) {
+            log.info("Polling");
+            // Poll kafka and get as many records as you can now, but if there are no records,
+            // wait 100 milliseconds to get records. If by 100 milliseconds no records have been received,
+            // go to the next line of code, records will be an empty collection.
+            // So, .poll either returns right away with records or waits up until timeout to return some records.
+            ConsumerRecords<String, String> records =
+                    consumer.poll(Duration.ofMillis(1000));
+
+            for (ConsumerRecord<String, String> record : records){
+                log.info("Key: " + record.key() + ", Value: " + record.value());
+                log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
+    }
+}
--- core/src/main/java/org/spyne/javapluginquickstart/core/Util.java
+++ core/src/main/java/org/spyne/javapluginquickstart/core/Util.java
@@ -3,15 +3,12 @@ package org.spyne.javapluginquickstart.core;
 import org.spyne.javapluginquickstart.spi.task.PluginFactory;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
 
 public class Util {
 
     public static PluginFactory loadPlugin(String pluginsPath, String pluginName){
         PluginLoader pluginLoader = new PluginLoader(new File(pluginsPath));
         pluginLoader.loadPlugins();
-
         return pluginLoader.getFooFactory(pluginName);
     }
 }
